e2636b389719fd9832ddb979dacef346a1ca0b09
Kushal K Dey <kshldey@gmail.com>
2016-01-18 02:35:46 -0600

    added packrat and also wrote example scripts for nonparametric smoothing

--- R/sin_cell_ordering_class.R
+++ R/sin_cell_ordering_class.R
@@ -23,17 +23,7 @@
 #'
 #' @author  Kushal K Dey
 #' @export
-#' @examples
-#'
-#' G <- 500;
-#' num_cells <- 400;
-#' amp_genes <- rep(10, G);
-#' phi_genes <- runif(G, 0, 2*pi)
-#' sigma_genes <- rchisq(G, 4);
-#' cell_times_sim <- sample(seq(0,2*pi, 2*pi/(num_cells-1)), num_cells, replace=FALSE);
-#' cycle_data <- sim_sinusoidal_cycle(G, amp_genes, phi_genes, sigma_genes, cell_times_sim);
-#' celltime_levels <- 100;
-#' out <- sin_cell_ordering_class(cycle_data, celltime_levels = 100, num_iter=100)
+
 
 sin_cell_ordering_class <- function(cycle_data, celltime_levels, num_iter, save_path=NULL,
                                   fix.phase=FALSE, phase_in=NULL)
--- test/np_example_2.R
+++ test/np_example_2.R
@@ -1,22 +1,29 @@
 
-## testing the code for nonparametric cell cycle re-ordering
-
 library(cellcycleR)
 library(wavethresh)
 
 G <- 100;
 num_cells <- 256;
-amp_genes <- rep(10, G);
-phi_genes <- rep(c(2,5), each=G/2);
-sigma_genes <- rchisq(G, 4);
+phi_genes <- seq(1, pi, length.out=G);
+sigma_genes <- rchisq(G, 1);
 cell_times_sim <- sort(sample(seq(0,2*pi, 2*pi/(num_cells-1)), num_cells, replace=FALSE));
-cycle_data <- sim_sinusoidal_cycle(G, amp_genes, phi_genes, sigma_genes, cell_times_sim);
+cycle_data <- matrix(0,num_cells,G)
+for(s in 1:num_cells){
+cycle_data[s,] <- 10*cell_times_sim[s] + 30*sin(2*cell_times_sim[s] + phi_genes) + rnorm(G,0,sigma_genes);
+}
+
+plot(cycle_data[,1], type="l")
+plot(cycle_data[,60], type="l")
+plot(cycle_data[,30], type="l")
+
 
 celltime_levels <- 128;
 
 sample_reorder <- sample(1:num_cells,num_cells, replace=FALSE);
 cell_times_reorder <- cell_times_sim[sample_reorder];
 cycle_data_reorder <- cycle_data[sample_reorder,];
+
+plot(cycle_data_reorder[,60], type="l")
 
 ###  B-spline smoothing
 
@@ -36,10 +43,18 @@ radial.plot(lengths=1:length(cell_times_reorder),radial.pos=sort(cell_times_reor
 
 plot(cycle_data_reorder[order(out$cell_times),1], type="l")
 plot(cycle_data[,1],type="l")
+plot(cycle_data_reorder[order(out$cell_times),7], type="l")
+plot(cycle_data[,7],type="l")
+plot(cycle_data_reorder[order(out$cell_times),60], type="l")
+plot(cycle_data[,60],type="l")
+plot(cycle_data_reorder[order(out$cell_times),80], type="l")
+plot(cycle_data[,80],type="l")
+
+
 
 ### LOWESS smoothing
 
-system.time(out <- np_cell_ordering_class(cycle_data_reorder, celltime_levels = 128, num_iter=100, method="LOWESS"))
+system.time(out <- np_cell_ordering_class(cycle_data_reorder, celltime_levels = 128, num_iter=100, method="LOESS"))
 
 
 ###### Post processing
@@ -74,6 +89,12 @@ radial.plot(lengths=1:length(cell_times_reorder),radial.pos=sort(cell_times_reor
 
 plot(cycle_data_reorder[order(out$cell_times),1], type="l")
 plot(cycle_data[,1],type="l")
+plot(cycle_data_reorder[order(out$cell_times),7], type="l")
+plot(cycle_data[,7],type="l")
+plot(cycle_data_reorder[order(out$cell_times),60], type="l")
+plot(cycle_data[,60],type="l")
+plot(cycle_data_reorder[order(out$cell_times),80], type="l")
+plot(cycle_data[,80],type="l")
 
 ### Sinusoidal smoothing
 
@@ -88,6 +109,13 @@ radial.plot(lengths=1:length(cell_times_reorder),radial.pos=sort(cell_times_reor
 
 plot(cycle_data_reorder[order(out$cell_times),1], type="l")
 plot(cycle_data[,1],type="l")
+plot(cycle_data_reorder[order(out$cell_times),7], type="l")
+plot(cycle_data[,7],type="l")
+plot(cycle_data_reorder[order(out$cell_times),60], type="l")
+plot(cycle_data[,60],type="l")
+plot(cycle_data_reorder[order(out$cell_times),80], type="l")
+plot(cycle_data[,80],type="l")
+
 
 
 
