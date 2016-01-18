3890eb0e54f2d3f7650942a1f298c894d95bc439
Kushal K Dey <kshldey@gmail.com>
2016-01-17 20:48:31 -0600

    added all three nonparameteric smoothers to the example script

--- test/np_example.R
+++ test/np_example.R
@@ -18,10 +18,12 @@ sample_reorder <- sample(1:num_cells,num_cells, replace=FALSE);
 cell_times_reorder <- cell_times_sim[sample_reorder];
 cycle_data_reorder <- cycle_data[sample_reorder,];
 
+###  B-spline smoothing
+
 system.time(out <- np_cell_ordering_class(cycle_data_reorder, celltime_levels = 100, num_iter=100, method="B-spline"))
 
 
-## Post processing
+###### Post processing
 
 plot(sigma_genes, out$sigma, col="red",xlab="true sigma", ylab="est sigma", main="sigma(variation) est, comparison")
 
@@ -35,3 +37,40 @@ radial.plot(lengths=1:length(cell_times_reorder),radial.pos=sort(cell_times_reor
 plot(cycle_data_reorder[order(out$cell_times),1], type="l")
 plot(cycle_data[,1],type="l")
 
+### LOWESS smoothing
+
+system.time(out <- np_cell_ordering_class(cycle_data_reorder, celltime_levels = 100, num_iter=100, method="LOWESS"))
+
+
+###### Post processing
+
+plot(sigma_genes, out$sigma, col="red",xlab="true sigma", ylab="est sigma", main="sigma(variation) est, comparison")
+
+library(plotrix)
+library(RColorBrewer)
+radial.plot(lengths=1:length(out$cell_times),radial.pos=out$cell_times[order(cell_times_reorder)],
+            line.col=colorRampPalette(brewer.pal(9,"Blues"))(length(out$cell_times)), lwd=2)
+radial.plot(lengths=1:length(cell_times_reorder),radial.pos=sort(cell_times_reorder),
+            line.col=colorRampPalette(brewer.pal(9,"Blues"))(length(cell_times_reorder)), lwd=2)
+
+plot(cycle_data_reorder[order(out$cell_times),1], type="l")
+plot(cycle_data[,1],type="l")
+
+### Wavelet smoothing
+
+system.time(out <- np_cell_ordering_class(cycle_data_reorder, celltime_levels = 100, num_iter=100, method="Wavelet"))
+
+
+###### Post processing
+
+plot(sigma_genes, out$sigma, col="red",xlab="true sigma", ylab="est sigma", main="sigma(variation) est, comparison")
+
+library(plotrix)
+library(RColorBrewer)
+radial.plot(lengths=1:length(out$cell_times),radial.pos=out$cell_times[order(cell_times_reorder)],
+            line.col=colorRampPalette(brewer.pal(9,"Blues"))(length(out$cell_times)), lwd=2)
+radial.plot(lengths=1:length(cell_times_reorder),radial.pos=sort(cell_times_reorder),
+            line.col=colorRampPalette(brewer.pal(9,"Blues"))(length(cell_times_reorder)), lwd=2)
+
+plot(cycle_data_reorder[order(out$cell_times),1], type="l")
+plot(cycle_data[,1],type="l")
